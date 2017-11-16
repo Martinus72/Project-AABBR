c43b244a688e31afda03499856aa6db98efa28a6
alexandre-goux <alexandre.goux.44@gmail.com>
2017-11-16 12:18:22 +0100

    Merge branch 'master' of https://github.com/Martinus72/Project-AABBR
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,10 +19,33 @@ public class Main {
         // Initialisation de la variable du menu
         int menuChoix = 0;
 
-        while(menuChoix!=10){
-             System.out.println(" ------------------------------------------------------------- \n" +
-                                " ");
+        Scanner scanner = new Scanner(System.in);
 
+        while(menuChoix!=10){
+
+            // Affichage du menu
+             System.out.println(" ------------------------------------------------------------- \n \n" +
+                                "                       Gestionnaire AABRR                      \n \n" +
+                                " 1) Réaliser un arbre à partir d'un fichier texte \n" +
+                                " 2) Réaliser un \n" +
+                                " 10) Quitter l'application \n \n" +
+                                " ------------------------------------------------------------- \n \n \n" +
+                                " Choix : ");
+             try {
+
+                 menuChoix = scanner.nextInt();
+
+             } catch(InputMismatchException e){
+
+                 System.out.println(" Merci d'indiquer uniquement un des choix du menu");
+             }
+
+             switch (menuChoix){
+                 case 1 :
+                     System.out.println("Choix 1");
+                 case 2 :
+                     System.out.println("Choix 2");
+             }
         }
 
     }
